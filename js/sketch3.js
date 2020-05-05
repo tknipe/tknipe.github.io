e6f3aabc3cd996f237ee309ac1c6097737ae879e
tknipe <tknipe@hotmail.co.uk>
2020-05-05 18:09:02 +0100

    V1.2
    -Cleaned up comments
    -Reduced txt stroke because export SVG has weird issue making it thicker when saved.
    -Fixed font slider having more values than fonts

--- js/sketch3.js
+++ js/sketch3.js
@@ -1,17 +1,13 @@
 //const CW = 630
 //const CH = 810
-
 var palette = [0]
-var test = [ [1,2,3,4], [5,6,7,8] ]
 var randcol = ''
 let fonts = [];
-
+// var pdf;
 var txtinput = 'The future is retro'
 var txtarea = ''
-//var pdf;
 let loopval = 0;
 let randval;
-//let randomizer;
 let loopslider;
 let slider1;
 let slider2;
@@ -22,24 +18,15 @@ function preload() {
   var font3 = loadFont('../css/fonts/Righteous-Regular.ttf');
   var font4 = loadFont('../css/fonts/Haarlem Deco DEMO.ttf');
   var font5 = loadFont('../css/fonts/StiffStaff-Heavy.ttf');
-  //var font6 = loadFont('../fonts/Bazar.ttf');
-
   fonts.push(font1,font2,font3,font4,font5)
 }
 
 function setup() {
+    //find DOM containers and set dimensions to the canvas container
     var maincontain = document.getElementById('maincontain')
     var CW = document.getElementById('canvascontainer').offsetWidth
     var CH = document.getElementById('canvascontainer').offsetHeight 
-    //canva.id('canvad')
-    //maincontain.appendChild(document.getElementById('canvas'))
-    // console.log(palette)
-    // console.log(test)
-    //console.log(String(palette[4]))
-
-    //controls container
     var container = document.getElementById('container')
-
     //textbox
     txtarea = createElement('textarea', sentence)
     txtarea.parent(container)
@@ -91,42 +78,17 @@ function setup() {
     s3label.parent(slidercontainer)
     s3label.style('margin-top', '32px')
     s3label.class('sliderlabel')
-    slider3 = createSlider(-1, floor(fonts.length), -1);
+    slider3 = createSlider(-1, floor(fonts.length)-1, -1);
     slider3.parent(slidercontainer)
     slider3.id('slidey3')
-    //canvas
+    //canvas creation and append to container
     var canvascontainer = document.getElementById('canvascontainer')
-
-
-
-    console.log(CW)
-    console.log(CH)
     createCanvas(CW,CH, SVG)
     canvascontainer.appendChild(document.getElementById('defaultCanvas0'))
-
-    pdf = createPDF();
-
-    pdf.beginRecord();
+    //pdf(doesn't work atm)
     // pdf = createPDF();
     // pdf.beginRecord();
-
-
-    // var pdfbtn = createButton('Print')
-    // pdfbtn.mousePressed(pdf.save())
-
-
-
-    // print( 32 * (pow(1.6, -2)) )
-    // print( 32 * (pow(1.6, -2.5)) )
-    // print( 48 * (pow(1.6, -3)) )
-    // print( 64 * (pow(1.6, -4)) )
-    // print( 86 * (pow(1.6, -4)) )
-    // print( 112 * (pow(1.6, -5)) )
-    // print( 148 * (pow(1.6, -6)) )
-    // print( 196 * (pow(1.6, -6.5)) )
-    // print( 256 * (pow(1.6, -7)) )
 }
-
 function randomizer() {
     txtarea.value(sentenceArray[floor(random()*sentenceArray.length)])
     slider1.value(random(24,256)) 
@@ -136,14 +98,17 @@ function windowResized() {
     resizeCanvas(document.getElementById('canvascontainer').offsetWidth, document.getElementById('canvascontainer').offsetHeight);
   }
 function draw() {
-
+    //set text from the textarea input
     txtinput = txtarea.value()
+    //choose palette & use to set bg 
     var palette = pals[floor(random()*pals.length)]
     background(palette[floor(random()*palette.length)])
-    //var txtsize = random(24,256)
+    ////var txtsize = random(24,256)
+    //set text size to slider value
     let val1 = slider1.value();
     var txtsize = val1
-
+    //all shapes are scaled at a golden ratio (*1.6ish) to the text
+    //this mess works out the range of "power factors" each group of text size can scale the shapes by (so that its not < ~10 or > ~650) 
     var powfac = 1;
     var powlac = 1;
     if (txtsize <= 32) { powfac = 6.5; powlac = -2; 
@@ -155,36 +120,18 @@ function draw() {
     } else if (txtsize > 148 && txtsize <= 196) {powfac = 2.5; powlac = -6.5;
     } else if (txtsize > 196 && txtsize <= 256) { powfac = 2; powlac = -7;
     } 
-    print(txtsize)
-    print(powfac)
-    print(powlac)
-    
-
-
+    //range of # of shapes decided by green slider
     let val2 = slider2.value()
-    // for (var i = 0; i < random(4); i++) {
+    //// for (var i = 0; i < random(4); i++) {
+    //shape generation
     for (var i = 0; i < random(val2); i++) {
-    
+    //random stroke weight, color (from palette), size (determined by txt and powfac) 
     strokeWeight( floor(random(0,3)) )
     fill(palette[floor(random()*palette.length)])
     ellipse(random(width), random(height), txtsize * pow( 1.6, random(powlac, powfac) ))
     rect(random(width), random(height), txtsize * pow( 1.6, random(powlac, powfac) ), txtsize * pow( 1.6, random(powlac, powfac) ))
     }
-    
-    // for (var i = 0; i < random(1); i++) {
-
-    //     beginShape();
-
-    //     for (var i = 0; i < random(4); i++) {
-        
-    //         curveVertex(txtsize * pow( 1.6, random(powlac, powfac) ), txtsize * pow( 1.6, random(powlac, powfac) ));
-
-    //     }
-    //     endShape(CLOSE);
-
-    // }
-    //var txtfnt = fonts[floor(random()*fonts.length)]; 
-    // = fonts[floor(random()*fonts.length)];
+    //font choice, if slider left its random or = slider value
     rectMode(CORNERS)
     stroke(0)
     if (slider3.value() == -1) {
@@ -192,55 +139,34 @@ function draw() {
     } else {
         txtfnt = fonts[floor(slider3.value())]
     }
-    //console.log(txtfnt)
-    
+    //stroke weight (on txt) determined by text size
     if( txtsize <= 42) {
         strokeWeight(2)
     }
     else if( txtsize <=64) {
-        strokeWeight(3)
+        strokeWeight(2)
     }
-    else {
-        strokeWeight(4)
+    else if (( txtsize > 64)) {
+        strokeWeight(2)
     }
     textFont(txtfnt)
-
+    //size of text box wrap
     textSize(txtsize)
     var wt = random(width)
     var ht = random(height)
     var xt = random(width-wt)
     var yt = random(height-ht)
-    //console.log(wt,ht,xt,yt)  
-
     text(txtinput, xt , yt, wt, ht)
+    //loop speed based on slider
     frameRate(loopval)
     loopslider.changed(loopdedoop)
     function loopdedoop(){
     loopval = loopslider.value()
     frameRate(loopval)
     }
-
-
-    // if (loopval = 0) {
-    //     //noLoop()
-    //     framerate(0)
-    // } else {
-    //     //loop()
-    //     frameRate(loopval)
-    // }
-    //frameRate(0)
-    // noLoop()
-    //save("mySVG.svg");
-    // if (frameCount == 1) {
-    //     noLoop();
-    //     pdf.save();
-    // }
-
+    //export PDF (or SVG apparently)
     function saveSVG() {
-        pdf.save();
+        save();
         }
-    // function printPDF() {
-    //     pdf.save();
-    //     }
 
 }
